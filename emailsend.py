1e149a92664532cff33f499c9b9a1ddf998e71fc
priyanka1113 <60529116+priyanka1113@users.noreply.github.com>
2023-08-25 17:07:53 -0400

    updated email sender files

--- emailsend.py
+++ emailsend.py
@@ -2,6 +2,7 @@ from smtplib
 from string import Template
 from pathlib import Path
 from email.message import EmailMessage
+
 html=Template(Path('emailindex.html').read_text())
 msg=EmailMessage()
 msg["from"]='priyanka Deo'
@@ -10,7 +11,9 @@ msg['Subject']="you have won 100000000 dollares!"
 
 #msg.set_content('I am master.I am the Prize!')
 #for dynamic content
-msg.set_content(html.substitute(name='priyanka'),'html')
+# msg.set_content(html.substitute(name='priyanka'),'html')
+msg.set_content(html.substitute({'name':'priyanka'}),'html')
+
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
